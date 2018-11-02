3ce000fad00e0bfca30b972ed82f114c62517935
Daniel Hamilton <danhamilton16@gmail.com>
2018-11-02 00:05:15 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,7 +2,7 @@ def reformat_languages(languages)
   reformatted={}
   languages.each do |style, stylevalues|
     stylevalues.each do |language, languagedata|
-      reformatted[language]={type:languagedata[:type],style:style}
+      reformatted[language]={type:languagedata[:type],style:[style]}
     end
   end
   return reformatted
